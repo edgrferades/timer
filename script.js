70aededaa1502f29f5ebadeb698ccfddd3693262
Edgar <edgarfernandes21@gmail.com>
2023-04-16 14:57:30 +0100

    project ready

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const timeHeading = document.querySelector('h1');
-let timeSecond = 3;
+let start = document.querySelector('#start');
+start.addEventListener('click', () => {
+    startTimer();
+})
 
-displayTime(timeSecond);
-
-const countDown = setInterval(() => {
-    timeSecond--;
-    displayTime(timeSecond)
-    if (timeSecond < 0 || timeSecond < 1) {
-        endTime();
-        clearInterval(countDown);
+let startTimer = () => {
+    const timeHeading = document.querySelector('h1');
+    let timeSecond = document.querySelector('#input').value;
+    
+    displayTime(timeSecond);
+    
+    const countDown = setInterval(() => {
+        timeSecond--;
+        displayTime(timeSecond)
+        if (timeSecond < 0 || timeSecond < 1) {
+            endTime();
+            clearInterval(countDown);
+        }
+    }, 1000);
+    
+    function displayTime(timeValue) {
+        const min = Math.floor(timeValue / 60);
+        const sec = Math.floor(timeValue % 60);
+        timeHeading.textContent = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`;
+    };
+    
+    let endTime = () => {
+        timeHeading.innerText = 'TIME OUT';
     }
-}, 1000);
-
-function displayTime(timeValue) {
-    const min = Math.floor(timeValue / 60);
-    const sec = Math.floor(timeValue % 60);
-    timeHeading.textContent = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`;
 };
-
-let endTime = () => {
-    timeHeading.innerText = 'TIME OUT';
-}
